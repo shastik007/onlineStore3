5881a50b78562eb06d8611c87dcb25c363607dfd
raatbek <jumabekovradik@gmail.com>
2022-09-24 18:43:08 +0600

    add total price when send email

--- src/main/java/online/db/servise/ProductService.java
+++ src/main/java/online/db/servise/ProductService.java
@@ -173,7 +173,7 @@ public class ProductService {
 
         for (ChildOrderDto order : orderDto.getOrders()) {
             Products product = productRepository.findById(order.getProductId()).orElseThrow();
-            totalPrice += product.getPrice();
+            totalPrice += product.getPrice() != null ? product.getPrice() : 0D;
             basket.append("Модел: ").append(product.getModel()).append(", Количество: ").append(order.getCount()).append(", Цена: ").append(product.getPrice()).append("\n");
         }
 
